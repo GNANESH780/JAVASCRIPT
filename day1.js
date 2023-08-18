548c1d9a6db064f62dcca197e7f97dcc91057fa7
GNANESH780 <gnaneshreddy780@gmail.com>
2023-08-18 15:46:03 +0530

    1stprogram

---
+++ day1.js
@@ -0,0 +1,12 @@
+< !doctype html >
+    <html>
+        <head>
+            <titie>js basics</titie>
+            <script>
+                console.log("hello worold");
+            </script>
+        </head>
+        <body>
+            <h1>js basics</h1>
+        </body>
+    </html>
